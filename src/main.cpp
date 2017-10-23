786e9a2b3fe92eec3571eac77ede50f11935504b
Samuel Rustan <samrustan@gmail.com>
2017-10-23 00:44:17 -0500

    only partially complete, not compiled, not tested.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
 
@@ -196,6 +197,12 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
 
+  // start in lane 1
+  int lane = 1;
+
+  // reference velocity to target
+  double ref_vel = 49.5; //mph
+
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -240,7 +247,96 @@ int main() {
 
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+            // note upto line 238 -code same as manuj's line 244
+            // upto here about the same as jshannon's too.
+            //
+
+            int prev_size = previous_path_x.size();
             
+            vector<double> ptsx;
+            vector<double> ptsy;
+
+            double ref_x = car_x;
+            double ref_y = car_y;
+            double ref_yaw = deg2rad(car_yaw);
+
+            if (prev_size < 2)
+            {
+              double prev_car_x = car_x - cos(car_yaw);
+              double prev_car_y = car_y - sin(car_yaw);
+
+              ptsx.push_back(prev_car_x);
+              ptsx.push_back(car_x);
+
+              ptsy.push_back(prev_car_y);
+              ptsy.push_back(car_y);
+            }
+            else
+            {
+              ref_x = previous_path_x[prev_size-1];
+              ref_y = previoud_path_y[prev_size-1];
+
+              double ref_x_prev = previoud_path_x[prev_size-1];
+              double ref_y_prev = previous_path_y[prev_size-1];
+              ref_yaw = atan2(ref_y-ref_y_prev,ref_x-ref_x_prev);
+
+              ptsx.push_back(ref_x_prev);
+              ptsx.push_back(ref_x);
+
+              ptsy.push_back(ref_y_prev);
+              ptsy.push_back(ref_y);
+
+            }
+
+            // in Frenet add 30m spaced points evenly ahead of the starting reference 
+            vector<double> next_wp0 = getXY(car_s+30,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+            vector<double> next_wp1 = getXY(car_s+60,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+            vector<double> next_wp2 = getXY(car_s+90,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+
+            ptsx.push_back(next_wp0[0]);
+            ptsx.push_back(next_wp1[0]);
+            ptsx.push_back(next_wp2[0]);
+
+            ptsx.push_back(next_wp0[1]);
+            ptsx.push_back(next_wp1[1]);
+            ptsx.push_back(next_wp2[1]);
+
+            for (int i = 0; i < ptsx.size, i++)
+            {
+              // shift car reference angle to 0deg
+              double shift_x = ptsx[i] - ref_x;
+              double shift_y = ptsy[i] - ref_y;
+
+              ptsx[i] = (shift_x * cos(0-ref_yaw) - shift_y * sin(0-ref_yaw);
+              ptsy[i] = (shift_x * sin(0-ref_yaw) - shift_y * cos(0-ref_yaw);
+
+            }
+
+            // spine
+            tk::spline s;
+
+            s.set_points(ptsx,ptsy);
+
+            // define the actual (x,y) for the planner
+          	vector<double> next_x_vals;
+          	vector<double> next_y_vals;
+
+            for (int i = 0; i < previous_path_x.size(); i++)
+            {
+              next_x_vals.push_back(previous_path_x[i]);
+              next_y_vals.push_back(previous_path_y[i]);
+            }
+
+            // calc how to set spline points in line with ref vel
+            double target_x = 30.0;
+            double target_y = s(target_x);
+            double target_dist = sqrt((target_x * target_x)+(target_y * target_y));
+
+            double x_add_on;
+
+            // TODO: finish up from video, ref video time 37:08
+            // upto this point is uncompiled and not tested at all
+
             
             double dist_inc = 0.5;
             for (int i = 0; i <50; i++)
@@ -253,7 +349,10 @@ int main() {
               next_y_vals.push_back(xy[1]);
             }
 
-            //END
+            // END --this is where mj's code starts to be the same
+            // mj's code is the same to the end.
+            //
+            // shannon's code same from here -besides some debugging code.
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
